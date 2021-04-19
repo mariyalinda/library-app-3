13dc37192f7766680b4dc5817cdb0d934ae2bfaa
linda <lindaliz5995@gmail.com>
2021-04-19 20:46:07 +0530

    add author routes

--- Frontend/src/app/authors/authors.component.ts
+++ Frontend/src/app/authors/authors.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { AuthorService } from '../author.service';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
-  styleUrls: ['./authors.component.css']
+  styleUrls: ['./authors.component.css'],
 })
 export class AuthorsComponent implements OnInit {
-
-  constructor() { }
+  authors = [
+    {
+      name: '',
+      image: '',
+      place: '',
+      genre: '',
+    },
+  ];
+  constructor(
+    public _auth: AuthService,
+    private authorservice: AuthorService,
+    private _router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.authorservice.getAuthors().subscribe((data) => {
+      this.authors = JSON.parse(JSON.stringify(data));
+    });
   }
 
+  getAuthor(author: any) {
+    //get individual book
+  }
+  editAuthor(author: any) {
+    localStorage.setItem('editAuthorId', author._id.toString());
+    this._router.navigate(['update/author']);
+  }
+  deleteAuthor(author: any) {
+    this.authorservice.deleteAuthor(author._id).subscribe((data) => {
+      this.authors = this.authors.filter((p) => p !== author);
+    });
+  }
 }
--- Frontend/src/app/authorupdate/authorupdate.component.ts
+++ Frontend/src/app/authorupdate/authorupdate.component.ts
@@ -1,15 +1,27 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthorService } from '../author.service';
 
 @Component({
   selector: 'app-authorupdate',
   templateUrl: './authorupdate.component.html',
-  styleUrls: ['./authorupdate.component.css']
+  styleUrls: ['./authorupdate.component.css'],
 })
 export class AuthorupdateComponent implements OnInit {
-
-  constructor() { }
+  author = {
+    name: '',
+    genre: '',
+    place: '',
+    about: '',
+    image: '',
+  };
+  constructor(private authorservice: AuthorService, private _router: Router) {}
 
   ngOnInit(): void {
+    let authorid = localStorage.getItem('editAuthorId');
+    this.authorservice.getAuthor(authorid).subscribe((data) => {
+      this.author = JSON.parse(JSON.stringify(data));
+    });
   }
-
 }
